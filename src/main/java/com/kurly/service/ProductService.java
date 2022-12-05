0397948d93a240243ccafc4778a33499fd8ad807
HMJ <73444109+HMyunJin@users.noreply.github.com>
2022-12-06 03:34:09 +0900

    카테고리 상품/신상품 페이지 추가

--- src/main/java/com/kurly/service/ProductService.java
+++ src/main/java/com/kurly/service/ProductService.java
@@ -11,4 +11,27 @@ public interface ProductService {
 	
 	// 재고가 5개 미만인 상품의 정보를 가져온다
 	public List<ProductVO> getSoonSoldOut() throws Exception;
+	
+	//카테고리에 종류에 맞는 상품 목록 가져오기
+	public List<ProductVO> categoryProduct(Map<String,Object> data) throws Exception;
+	
+	//카테고리코드에 맞는 메인카테고리 정보 가져오기
+	public CategoryVO mainCategory(String category) throws Exception;
+	
+	//카테고리에 맞는 상품의 브랜드 가져오기
+	public List<BrandVO> companyFilter(Map<String,Object> data) throws Exception; 
+	
+	//상품 개수 세기
+	public int countProduct(Map<String,Object> data) throws Exception;
+	
+	//신상품
+	//신상품 목록
+	public List<ProductVO> newProduct(Map<String,Object> data) throws Exception;
+	
+	//신상품 개수 세기
+	public int countNewProduct(Map<String,Object> data) throws Exception;
+	
+	//메인 카테고리 목록 가져오기
+	public List<CategoryVO> categoryFilter() throws Exception;
+	
 }
